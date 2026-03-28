3bc57f02038c4d18d00327a6618641a631d1aace
PilliGoutham <134474542+PilliGoutham@users.noreply.github.com>
2026-03-28 10:58:51 +0530

    Add Test class with checkMethod and main method

--- testprogram.java
+++ testprogram.java
@@ -1,1 +1,10 @@
-
+Public class Test{
+  public int checkMethod(){
+    System.out.println("first Method");
+    return 1;
+  }
+}
+public static void Main(String args[]){
+  Test createObject = new Test();
+  createObject.checkMethod();
+}
